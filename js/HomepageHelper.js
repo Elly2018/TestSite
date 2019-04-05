a945c013797da0316e8467ec9dd6ae27b71534e8
Elly2018 <34325707+Elly2018@users.noreply.github.com>
2019-04-05 17:50:03 +0800

    SmartBoi

--- js/HomepageHelper.js
+++ js/HomepageHelper.js
@@ -1,15 +1,16 @@
 // The script that response for the web page rendering
 function RDonload(){
-    $.getJSON("../media/image/Titlework/workheader.json", function(data){
-    data = $.parseJSON(data);
-    $.each(data, function(index, value){
-        alert(value);
-    })
-    }).done(function(){
-    })
-    .fail(function(){
-    })
+    
 }
 function RDOnTItleWork(){
 
+}
+
+function ImageZoomIn(e){
+    var d = e.target;
+    
+}
+
+function ImageZoomOut(e){
+    var d = e.target;
 }
